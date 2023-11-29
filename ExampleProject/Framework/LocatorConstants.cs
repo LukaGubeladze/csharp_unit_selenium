3a445afc9e99918d6c1aab765ea689bd7f545edb
Nikita Shinov <n.shinov@a1qa.com>
2023-11-29 14:29:26 +0100

    Revert "added tasks"
    This reverts commit b22780272e140a94a2a2f6f13203f6de5cd473dc.

--- ExampleProject/Framework/LocatorConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExampleProject.Framework
-{
-    public class LocatorConstants
-    {
-        public const string PreciseTextLocator = "//*[text()='%s']";
-        public const string PartialTextLocator = "//*[contains(text(),'%s')]";
-    }
-}
--- ExampleProject/Framework/Tests/FileUploadTests.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExampleProject.Framework.Tests
-{
-    internal class FileUploadTests : BaseTest
-    {
-        private static readonly string fileName = testdata.GetValue<string>("fileDownload.folderPath");
-        private static readonly string filePath = testdata.GetValue<string>("fileDownload.fileName") + fileName;
-    }
-}
